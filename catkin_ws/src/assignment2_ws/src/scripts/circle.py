3187135e380e570e8edf4bf3454cc2233d1bbe27
Ryan Nguyen <rnguye2@clemson.edu>
2021-01-26 01:18:01 -0500

    added my python scripts

--- catkin_ws/src/assignment2_ws/src/scripts/circle.py
+++ catkin_ws/src/assignment2_ws/src/scripts/circle.py
@@ -2,6 +2,8 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+import time
 
 class TurtleBot:
 
@@ -13,21 +15,28 @@ class TurtleBot:
 		# Publisher which will publish to the topic '/turtle1/cmd_vel'.
 		self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel',
 				                          Twist, queue_size=10)
-	def move_circle(r):
-
-		move_cmd = Twist()
-		move_cmd.linear.x = 1.0
-		move_cmd.angular.z = 1.0
+				                    						
+	
+		self.rate = rospy.Rate(10)
 
 
-		rate = rospy.Rate(10)
+	def circle(self):
+		time_duration = 5
+		time_start = time.time()
+		
+		while time.time() < time_start + time_duration:
+			move_cmd = Twist()
+			move_cmd.linear.x = 10.0
+			move_cmd.angular.z = move_cmd.linear.x/rospy.get_param('~r')
 
+
+
+			self.velocity_publisher.publish(move_cmd)
 
 if __name__ == '__main__':
 	try:
 		x = TurtleBot()
-		r = 1
-		x.move_circle()
+		x.circle()
 	except rospy.ROSInterruptException:
 		pass
 
