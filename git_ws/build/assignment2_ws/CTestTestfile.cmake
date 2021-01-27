8ba130c41e8301747e20ed4410f0009572f7560a
Ryan Nguyen <rnguye2@clemson.edu>
2021-01-27 17:34:30 -0500

    deleting everything

--- git_ws/build/assignment2_ws/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rnguye2/git_ws/AuE893Spring21_RyanNguyen/git_ws/src/assignment2_ws
-# Build directory: /home/rnguye2/git_ws/AuE893Spring21_RyanNguyen/git_ws/build/assignment2_ws
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
