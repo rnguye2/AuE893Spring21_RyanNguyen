dcd2bd0e4d783d138bff0ab19323ac8217c9d5ca
Ryan Nguyen <rnguye2@clemson.edu>
2021-01-27 18:00:42 -0500

    delete everything

--- ros_ws/README.md
+++
@@ -1,4 +0,0 @@
-Hello my name is Ryan Nguyen and I am an automotive engineering Ph.D student working with Dr. 
-Rahul Rai.
-
-This repository will contain projects from this semester for AuE893: Autonomy: science and Systems.
--- ros_ws/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/rnguye2/git_ws/AuE893Spring21_RyanNguyen/ros_ws/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
